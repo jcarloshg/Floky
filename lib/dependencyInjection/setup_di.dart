0b2c619a8757557f234656b133010453bb1f7eaf
jcarloshg <carlosj12336@gmail.com>
2022-08-26 18:41:21 -0500

    ADD to [setupDI] with registerSingleton [DataStDream]

--- lib/dependencyInjection/setup_di.dart
+++ lib/dependencyInjection/setup_di.dart
@@ -1,1 +1,11 @@
-Future<void> setupDI() async {}
+import 'package:get_it/get_it.dart';
+
+class DataStream {
+  static String data = 'class _DataStream';
+}
+
+GetIt di = GetIt.instance;
+
+Future<void> setupDI() async {
+  di.registerSingleton<DataStream>(DataStream());
+}
