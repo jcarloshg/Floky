461c3b8e83d07b05083714d6a553483e7d4e618f
jcarloshg <carlosj12336@gmail.com>
2022-08-27 21:21:00 -0500

    IMPLEMENT && CREATE [Student]

--- lib/domain/entities/student.entitie.dart
+++ lib/domain/entities/student.entitie.dart
@@ -1,19 +1,33 @@
 class Student {
-  final String id;
-  final String name;
-  final String lastName;
-  final String emial;
-  final String picture;
-  final String registerCollege;
-  final String college;
+  final String _id;
+  final String _college;
+  final String _emial;
+  final String _lastName;
+  final String _name;
+  final String _picture;
+  final String _registerCollege;
 
   Student(
-    this.id,
-    this.name,
-    this.lastName,
-    this.emial,
-    this.picture,
-    this.registerCollege,
-    this.college,
+    this._id,
+    this._college,
+    this._emial,
+    this._lastName,
+    this._name,
+    this._picture,
+    this._registerCollege,
   );
+
+  Map toJson() => {
+        'id': _id,
+        'college': _college,
+        'emial': _emial,
+        'lastName': _lastName,
+        'name': _name,
+        'picture': _picture,
+        'registerCollege': _registerCollege,
+      };
+
+  static getVoidStudent() {
+    return Student('', '', '', '', '', '', '');
+  }
 }
--- lib/views/pages/authenticate/singup/singup.provider.dart
+++ lib/views/pages/authenticate/singup/singup.provider.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
 import 'package:floky/domain/entities/entities.index.dart';
 import 'package:flutter/material.dart';
 
 class SingUpProvider extends ChangeNotifier {
+  Student student = Student.getVoidStudent();
+
   void printStudent() {
     // ignore: avoid_print
     print(';laskdf;lsakdjf');
+    // ignore: avoid_print
+    print(json.encode(student));
   }
 }
