cc4a782c6cd628a992ac735145ffce8c703d3cad
jcarloshg <carlosj12336@gmail.com>
2022-09-05 17:07:04 -0500

    IMPLEMENT [AuthenticateBloc] into

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:floky/data/repository/configure_amplify.dart';
 import 'package:floky/dependencyInjection/setup_di.dart';
+import 'package:floky/domain/bloc/authenticate/authenticate_bloc.dart';
 import 'package:floky/views/pages/pages.index.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
-  await setupDI();
   WidgetsFlutterBinding.ensureInitialized();
+  await setupDI();
   runApp(const MyApp());
 }
 
@@ -25,12 +28,17 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: PageIndex.getAppRoutes(),
-      initialRoute: PageIndex.initialRoute,
-      title: 'Floky',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthenticateBloc>(create: (_) => di<AuthenticateBloc>()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: PageIndex.getAppRoutes(),
+        initialRoute: PageIndex.initialRoute,
+        title: 'Floky',
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+      ),
     );
   }
 }
