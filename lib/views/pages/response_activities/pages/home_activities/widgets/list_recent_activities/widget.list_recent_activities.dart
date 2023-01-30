c8b09539e6cf31d138a0f16bda5fc0902ce80b2d
jcarloshg <carlosj12336@gmail.com>
2023-01-30 17:44:50 -0600

    create [ListRecentActivities]

--- lib/views/pages/response_activities/pages/home_activities/widgets/list_recent_activities/widget.list_recent_activities.dart
+++ lib/views/pages/response_activities/pages/home_activities/widgets/list_recent_activities/widget.list_recent_activities.dart
@@ -1,46 +1,57 @@
 import 'package:floky/dependencyInjection/setup_di.dart';
+import 'package:floky/domain/entities/models/ModelProvider.dart';
 import 'package:floky/views/pages/response_activities/controller.response_activities.dart';
 import 'package:floky/views/widgets/widgets.index.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class ListRecentActivities extends StatelessWidget {
-  final String title = 'Descubre actividades nuevas';
-
+class ListRecentActivities extends StatefulWidget {
   const ListRecentActivities({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    activitiesCarousel();
+  State<ListRecentActivities> createState() => _ListRecentActivitiesState();
+}
 
+class _ListRecentActivitiesState extends State<ListRecentActivities> {
+  final String _title = 'Descubre actividades nuevas';
+  List<Activity> _activities = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getRecentActivities();
+  }
+
+  Future<void> getRecentActivities() async {
+    final responseActivitiesController = di<ResponseActivitiesController>();
+    final activities = await responseActivitiesController.getRecentActivities();
+    setState(() => _activities = activities);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(border: Border.all()),
-      // margin: const EdgeInsets.symmetric(horizontal: 15),
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Titles.subtitle(title),
-        ],
-      ),
+      child: _activities.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Titles.subtitle(_title),
+                SizedBox(
+                  height: 100,
+                  child: activitiesCarousel(_activities),
+                )
+              ],
+            ),
     );
   }
 
-  Future<void> activitiesCarousel() async {
-    final responseActivitiesController = di<ResponseActivitiesController>();
-    final activities = await responseActivitiesController.getRecentActivities();
-    for (var element in activities) {
-      // ignore: avoid_print
-      print(element.name);
-    }
-    // return Container(
-    //   decoration: BoxDecoration(border: Border.all()),
-    //   width: 150,
-    //   height: 150,
-    //   child: ListView.separated(
-    //     itemCount: activities.length,
-    //     separatorBuilder: (_, __) => const SizedBox(width: 15, height: 15),
-    //     itemBuilder: (context, index) => Text(activities[index].name),
-    //   ),
-    // );
+  Widget activitiesCarousel(List<Activity> activities) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemCount: activities.length,
+      separatorBuilder: (_, __) => const SizedBox(width: 15, height: 15),
+      itemBuilder: (context, index) => Text(activities[index].name),
+    );
   }
 }
