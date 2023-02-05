4ca7f14960bc36ab9c2dc69cd4ceed52dfbe032a
jcarloshg <carlosj12336@gmail.com>
2023-02-04 20:16:08 -0600

    create && implements [Themes]

---
+++ lib/views/utils/themes/theme.light.dart
@@ -0,0 +1,16 @@
+import 'package:floky/views/utils/utils.index.dart';
+import 'package:flutter/material.dart';
+
+class Themes {
+  static ThemeData lightTheme() => ThemeData(
+        primarySwatch: Colors.blueGrey,
+        backgroundColor: ColorsApp.greyEEEEEE,
+        scaffoldBackgroundColor: ColorsApp.greyEEEEEE,
+      );
+
+  static ThemeData darkTheme() => ThemeData(
+        primarySwatch: Colors.blueGrey,
+        backgroundColor: ColorsApp.greyEEEEEE,
+        scaffoldBackgroundColor: ColorsApp.greyEEEEEE,
+      );
+}
