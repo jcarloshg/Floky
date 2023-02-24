d50072033d2dafd6ab3699ba903d4e5e11cebdad
jcarloshg <carlosj12336@gmail.com>
2023-02-24 09:46:35 -0600

    fix [initState()] into [MainScreen]

--- lib/views/pages/common/main_screen/screen.main.dart
+++ lib/views/pages/common/main_screen/screen.main.dart
@@ -1,3 +1,4 @@
+import 'package:floky/views/widgets/widgets.index.dart';
 import 'package:flutter/material.dart';
 import 'package:floky/dependencyInjection/setup_di.dart';
 import 'package:floky/views/pages/authenticate/pages/log_in/controller/controller.exist_a_student_logged_in.dart';
@@ -12,14 +13,14 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   void initState() {
-    checkExistStudentLoggedIn();
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) => checkExistStudentLoggedIn(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // return _existStudentLoggedIn ? const HomeScreen() : const LogInScreen();
-
     final existAStudentLoggedInController =
         di<ExistAStudentLoggedInController>();
     existAStudentLoggedInController.navigator.setBuildContext(context);
@@ -27,7 +28,9 @@ class _MainScreenState extends State<MainScreen> {
     return const SizedBox(
       height: double.infinity,
       width: double.infinity,
-      child: Text('FLOKY'),
+      child: Center(
+        child: ImageLogo(),
+      ),
     );
   }
 
