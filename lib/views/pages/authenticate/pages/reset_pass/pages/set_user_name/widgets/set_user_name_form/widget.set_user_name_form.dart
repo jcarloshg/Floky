757424746f35e411fbdc7a1f8ac24d0d0a61b4cd
jcarloshg <carlosj12336@gmail.com>
2023-02-24 18:48:12 -0600

    implement [di<ResetPassChangeNotifier>()]

--- lib/views/pages/authenticate/pages/reset_pass/pages/set_user_name/widgets/set_user_name_form/widget.set_user_name_form.dart
+++ lib/views/pages/authenticate/pages/reset_pass/pages/set_user_name/widgets/set_user_name_form/widget.set_user_name_form.dart
@@ -1,3 +1,6 @@
+import 'package:floky/dependencyInjection/setup_di.dart';
+import 'package:floky/views/pages/authenticate/pages/reset_pass/controllers/reset_pass/change_notifier.reset_pass.dart';
+import 'package:floky/views/pages/authenticate/widgets/input_email/input.email.dart';
 import 'package:floky/views/widgets/widgets.index.dart';
 import 'package:flutter/material.dart';
 
@@ -6,19 +9,20 @@ class SetUserNameForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final control = di<ResetPassChangeNotifier>();
+    final data = control.getResetPassData();
+    final form = data.getNewFormControl();
+
     return Form(
+      key: form.formKey,
       child: Container(
-        decoration: BoxDecoration(border: Border.all()),
         margin: EdgeInsets.symmetric(
           horizontal: Spacers.size15,
           vertical: Spacers.size10,
         ),
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(border: Border.all()),
-              height: 50,
-            )
+            EmailInput(control: form.userNameController),
           ],
         ),
       ),
